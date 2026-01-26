1a1cdd7bea2060c5862289ebf3ed94ef38c53350
Alyssa Evans <alyda@me.com>
2026-01-26 11:27:25 +0000

    benchmark with real input

--- aoc-2024-12-01/benches/aoc_bench.rs
+++ aoc-2024-12-01/benches/aoc_bench.rs
@@ -1,43 +1,44 @@
-use aoc_2024_12_01::{SAMPLE_INPUT, process_c_qsort, process_rust_sort, process_part_2, process_part_2_hashmap, process_part_2_uthash};
+use aoc_2024_12_01::{process_c_qsort, process_rust_sort, process_part_2, process_part_2_hashmap, process_part_2_uthash};
 #[cfg(feature = "glib")]
 use aoc_2024_12_01::process_part_2_glib;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+const REAL_INPUT: &'static str = include_str!("./REAL_INPUT.txt");
 
 fn benchmark_process_c_qsort(c: &mut Criterion) {
     c.bench_function("process c qsort", |b| {
-        b.iter(|| process_c_qsort(black_box(SAMPLE_INPUT)))
+        b.iter(|| process_c_qsort(black_box(REAL_INPUT)))
     });
 }
 
 fn benchmark_process_rust_sort(c: &mut Criterion) {
     c.bench_function("process rust sort", |b| {
-        b.iter(|| process_rust_sort(black_box(SAMPLE_INPUT)))
+        b.iter(|| process_rust_sort(black_box(REAL_INPUT)))
     });
 }
 
 fn benchmark_process_part_2(c: &mut Criterion) {
     c.bench_function("part 2 naive", |b| {
-        b.iter(|| process_part_2(black_box(SAMPLE_INPUT)))
+        b.iter(|| process_part_2(black_box(REAL_INPUT)))
     });
 }
 
 fn benchmark_process_part_2_hashmap(c: &mut Criterion) {
     c.bench_function("part 2 hashmap", |b| {
-        b.iter(|| process_part_2_hashmap(black_box(SAMPLE_INPUT)))
+        b.iter(|| process_part_2_hashmap(black_box(REAL_INPUT)))
     });
 }
 
 #[cfg(feature = "glib")]
 fn benchmark_process_part_2_glib(c: &mut Criterion) {
     c.bench_function("part 2 glib", |b| {
-        b.iter(|| process_part_2_glib(black_box(SAMPLE_INPUT)))
+        b.iter(|| process_part_2_glib(black_box(REAL_INPUT)))
     });
 }
 
 fn benchmark_process_part_2_uthash(c: &mut Criterion) {
     c.bench_function("part 2 uthash", |b| {
-        b.iter(|| process_part_2_uthash(black_box(SAMPLE_INPUT)))
+        b.iter(|| process_part_2_uthash(black_box(REAL_INPUT)))
     });
 }
 
